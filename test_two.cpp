228277a7ee58e25bcabdd59307770477f3d23a8b
ron1x1-abba <63398078+ron1x1-abba@users.noreply.github.com>
2020-10-13 18:51:04 +0300

    Update test_two.cpp

--- test_two.cpp
+++ test_two.cpp
@@ -5,8 +5,8 @@ int main() {
 	a.makeAllocator(2048);
 	char* pointer = a.alloc(5000);
 	if (pointer)
-		std::cout << "OK Size is controlled well!" << std::cout;
+		std::cout << "OK Size is controlled well!" << std::endl;
 	else
-		std::cout << "Bad! Smth wrong with size controll!" << std::cout;
+		std::cout << "Bad! Smth wrong with size controll!" << std::endl;
 	return 0;
 }
